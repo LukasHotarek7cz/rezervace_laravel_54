7f3b7c0308e64a87bfa10b94c2ac48e78ac99975
Lukáš Hotárek <l.hotarek7777@seznam.cz>
2017-05-10 12:52:07 +0200

    ČEŠTINA
    HOTOVO - FUNGUJE
    - přidána čeština
    
    DÁLE SE PRACUJE NA
    - js --> jQuery
    - lepší CSS
    - přidání auth
    - uprava vlidaci u SQL
    - uprava SQL

---
+++ laravel 54/resources/lang/cs/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    //'failed' => 'These credentials do not match our records.',
+    'failed' => 'Tyto přihlašovací údaje neodpovídají žadnému záznamu.',
+    //'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'Příliš mnoho pokusů o přihlášení. Zkuste to prosím znovu za :seconds vteřin.',
+
+
+];
---
+++ laravel 54/resources/lang/cs/passwords.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    //'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'password' => 'Heslo musí obsahovat alespoň 6 znaků a musí se shodovat s ověřením.',
+    //'reset' => 'Your password has been reset!',
+    'reset' => 'Heslo bylo obnoveno!',
+    //'sent' => 'We have e-mailed your password reset link!',
+    'sent' => 'E-mail s instrukcemi k obnovení hesla byl odeslán!',
+    //'token' => 'This password reset token is invalid.',
+    'token' => 'Klíč pro obnovu hesla je nesprávný.',
+    //'user' => "We can't find a user with that e-mail address.",
+    'user' => 'Nepodařilo se najít uživatele s touto e-mailovou adresou.',
+
+];
